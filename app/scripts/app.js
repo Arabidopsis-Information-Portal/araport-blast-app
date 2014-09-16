4b190feec0c5d1a47db3bafbd80c15e1abb24749
Steve Mock <mocksteve@gmail.com>
2014-09-16 16:12:48 -0500

    Working for all 5 blast app types without all the hard-coding locking it to me.
    You still have to paste in your bearer token, unfortunately.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,18 @@
 (function(window, $, undefined) {
     'use strict';
 
-
+  //the different blast types and their app IDs
+  var blastTypes = {
+    'blastn' : 'ncbi-blastn-2.2.29u5',
+    'blastp':'ncbi-blastp-2.2.29u3',
+    'blastx':'ncbi-blastx-2.2.29u3',
+    'tblastn':'ncbi-tblastn-2.2.29u3',
+    'tblastx':'ncbi-tblastx-2.2.29u3',
+  };
+  
   //app template
   var BLASTN = {
-        'name': 'blastn-test-%DATESTAMP',
+        'name': 'blast-test-%DATESTAMP',
         'appId': 'ncbi-blastn-2.2.29u3',
         'queue': 'normal',
         'nodeCount': 1,
@@ -35,12 +43,12 @@
         }
     };
 
-    console.log('Hello AIP Science App!');
-
     //should be done for me by Agave stuff
     var Agave = {};
     Agave.token = '';
     Agave.baseUrl = 'https://api.araport.org/';
+    Agave.profiles = Agave.baseUrl + 'profiles/v2/';
+    Agave.me = Agave.profiles + 'me/';
     Agave.files   = Agave.baseUrl + 'files/v2/media/system/';
     Agave.header  = {Authorization: 'Basic: ' + Agave.token};
     Agave.jobs    = Agave.baseUrl + 'jobs/v2/';
@@ -49,37 +57,57 @@
     Agave.errorStates    = ['KILLED', 'FAILED', 'STOPPED','ARCHIVING_FAILED'];
     Agave.finishedStates = ['FINISHED'];
 
-    var Blastn = {};
+    var Blastn = {}; //info dumping ground
+    //this file is the list of available installed databases to blast against
     Blastn.databaseListing = Agave.files + 'araport-compute-00-storage/blast/index.json';
+//    Blastn.databases = {};
 
-
-
-    
+    //using date and time as an identifier. probably should switch this to a uuid or something   
     if (!Date.now) {
         Date.now = function() { return new Date().getTime(); };
     }
     var now = Date.now();
-    console.log('now' + now);
     BLASTN.archivePath = BLASTN.archivePath.replace('%DATESTAMP',now);
     BLASTN.name = BLASTN.name.replace('%DATESTAMP',now);
 
-    //$('#token').val(Agave.token);
-    //use profile to look up the Agave username
-    //todo - this^
-
     //this should be done for me already too
-    Agave.username = 'mock';
-    BLASTN.archivePath = BLASTN.archivePath.replace('%USERNAME',Agave.username);
-    BLASTN.uploadPath  = Agave.files +'araport-storage-00/' + Agave.username + '/';
-    BLASTN.outputFile = Agave.files + 'araport-storage-00/' + Agave.username + '/archive/jobs/blastn-test-'+ now + '/blastn_out';
-    console.log('BLASTN.uploadPath' + BLASTN.uploadPath);
+    Agave.username = '';
 
 
-    BLASTN.checkJobStatus = function(token, jobId) {
-        console.log('BLASTN.checkJobStatus ' + token + ' : ' + jobId);
-        console.log('BLASTN.status = ' + BLASTN.status);
-        if(Agave.runningStates.indexOf(BLASTN.status) >= 0) {
-            
+    //get the user's profile info, mostly for the username
+    Blastn.getProfile = function(token) {
+        console.log('Blastn.getProfile called');
+        if(Agave.username.length <= 0) {
+            var jxhr = $.ajax({
+                headers: {
+                    'Authorization': 'Bearer ' + Agave.token
+                },
+                type: 'GET',
+                url: Agave.me,
+                dataType: 'json',  
+
+            }).done(function(json) {
+                Agave.username = json.result.username;
+                BLASTN.archivePath = BLASTN.archivePath.replace('%USERNAME',Agave.username);
+                Blastn.uploadPath  = Agave.files +'araport-storage-00/' + Agave.username + '/';
+                
+                console.log(Agave.username);
+            }).error(function(err) {
+                Blastn.status = 'ERROR';
+                //todo - something useful here like error handling or something
+                console.log('Unable to look up username ' + err);
+            });
+        } else {
+            console.log('profiles not called ' + Agave.username.length);
+        }
+    };
+  
+    //  
+    Blastn.checkJobStatus = function(token, jobId) {
+        console.log('Blastn.checkJobStatus ' + token + ' : ' + jobId);
+        console.log('Blastn.status = ' + Blastn.status);
+        if(Agave.runningStates.indexOf(Blastn.status) >= 0) {
+            $('.job-status').html('Checking...');
             $.ajax( {
                   headers: {
                     'Authorization': 'Bearer ' + token
@@ -89,40 +117,38 @@
                 dataType: 'json',       
             }).done(function(json) {
                 console.log('checked job status, it is ' + json.result.status);
-                BLASTN.status = json.result.status;
-                $('.job-status').html('Job Status is ' + BLASTN.status);
-                if(Agave.runningStates.indexOf(BLASTN.status) >= 0) {
-                    console.log('ok, here we go again because BLASTN.status =' + BLASTN.status);
-                    setTimeout(function() { BLASTN.checkJobStatus(Agave.token, BLASTN.jobId); }, 10000);
-                } else if(Agave.errorStates.indexOf(BLASTN.status) >= 0) {
-                    console.log('found ' + BLASTN.status + ' in Agave.errorStates ' + Agave.errorStates + ' with indexOf=' + Agave.errorStates.indexOf(BLASTN.status));
-                    BLASTN.jobError();
-                } else if(Agave.finishedStates.indexOf(BLASTN.status) >=0) {
-                    BLASTN.jobFinished(json.result);
+                Blastn.status = json.result.status;
+                $('.job-status').html(Blastn.status);
+                if(Agave.runningStates.indexOf(Blastn.status) >= 0) {
+                    console.log('ok, here we go again because Blastn.status =' + Blastn.status);
+                    setTimeout(function() { Blastn.checkJobStatus(Agave.token, Blastn.jobId); }, 10000);
+                } else if(Agave.errorStates.indexOf(Blastn.status) >= 0) {
+                    console.log('found ' + Blastn.status + ' in Agave.errorStates ' + Agave.errorStates + ' with indexOf=' + Agave.errorStates.indexOf(Blastn.status));
+                    Blastn.jobError();
+                } else if(Agave.finishedStates.indexOf(Blastn.status) >=0) {
+                    Blastn.jobFinished(json.result);
                 } else {
-                    console.log('unknown job state! =' + BLASTN.status); 
+                    console.log('unknown job state! =' + Blastn.status); 
                 }
             }).error(function() {
-                BLASTN.status = 'ERROR';
+                Blastn.status = 'ERROR';
                 //todo - something useful here like error handling or something
             });
         }
     };
     
-    BLASTN.jobFinished = function(result) {
+    Blastn.jobFinished = function(result) {
         console.log('Yay! Job is really really finished!');
-        BLASTN.result = result;
-        console.log(BLASTN.result);
+        Blastn.result = result;
+        console.log(Blastn.result);
+        console.log('getting output file ' + Blastn.outputFile);
         //now go get the output file and stuff it into the page
-         //Go fetch the output file
         var jxhr = $.ajax({
             headers: {
                 'Authorization': 'Bearer ' + Agave.token
             },
             type: 'GET',
-            url: BLASTN.outputFile,
-            //dataType: 'json',  
-
+            url: Blastn.outputFile,
         }).done(function(output) { 
             //stuff resulting info into page
             console.log(output);
@@ -130,15 +156,17 @@
         })
         .fail(function() {
             console.log('Could not retrieve output from job.');
-        });
- 
-        
+        });      
     };
-    BLASTN.jobError = function() {
+    Blastn.jobError = function() {
         console.log('Boo! Job is in an error state!');
     };    
 
-    BLASTN.getDatabases = function() {
+    Blastn.getDatabases = function() {
+        console.log('getDatabases');
+        console.log(Blastn.databases);
+        console.log(!Blastn.databases);
+        if(Blastn.databases) { return; }
         //Go fetch the list of available databases to blast against
         var jxhr = $.ajax({
             headers: {
@@ -149,14 +177,15 @@
             dataType: 'json',  
 
         }).done(function(json) { 
-            console.log( json );
+            Blastn.databases = json;
+            //console.log( json );
             //todo - check for empty databases response
             var nukes = '';
             var peps  = '';
             json.databases.forEach(
                 function(el, index, array) {
-                    console.log(el);
-                    var dbEl = '<div class="checkbox"><label><input type="checkbox" value="'+el.filename+'">'+el.label+'</label></div>';
+                    //console.log(el);
+                    var dbEl = '<div class="checkbox"><label><input type="checkbox" class="blast-database" value="'+el.filename+'">'+el.label+'</label></div>';
                     if(el.type === 'nucleotide') {
                         nukes += dbEl;
                     } else {
@@ -177,11 +206,14 @@
     };
     
     $('#token').on('paste change',function (event) {
+        console.log('paste or change');
+        console.log(event.type);
         setTimeout(function(e) {
             if(($('#token').val().length > 0) && (Agave.token != $('#token').val())) {
                 Agave.token = $('#token').val();
             }
-            BLASTN.getDatabases();
+            Blastn.getProfile(Agave.token);
+            Blastn.getDatabases();
         }, 100);
     });
         
@@ -193,20 +225,37 @@
 
     //gather the items that have changed in the advanced form and add just those for the app submit
     $('.form-submit').click(function (event) {
-        //gather advanced options
-        event.preventDefault();
-        
+        event.preventDefault();//stop form submission
+
         //grab the Agave token from the form (this has to change eventually as well)
         if($('#token').val().length > 0) {
             Agave.token = $('#token').val();
         }
         
+        //grab changed advanced options
         var changedAdvOptions = $('.advanced-blast-options').find('.form-changed');
-        console.log('length is ' + changedAdvOptions.length);
         for(var i =0; i < changedAdvOptions.length; i++) {
           console.log(changedAdvOptions[i].id + ' = ' + changedAdvOptions[i].value);
           BLASTN['parameters'][changedAdvOptions[i].id] = changedAdvOptions[i].value;
-        }        
+        }
+        
+        //get blast type and add to app instance
+        Blastn.blastType = $('#appId').val();
+        BLASTN.appId = blastTypes[Blastn.blastType];
+        Blastn.outputFile = Agave.files + 'araport-storage-00/' + Agave.username + '/archive/jobs/blastn-test-'+ now + '/' + Blastn.blastType + '_out';
+        
+        //get databases and add to app instance
+        var dbs = '';
+        $('.blast-database:checked').each(function(){
+            dbs +=$(this).val() + ' ';
+        });
+        if(dbs.length > 0) {
+            BLASTN.parameters.database = dbs;
+        } else {
+            //todo error saying you have to select at least one DB
+        }
+                      
+        console.log(BLASTN);       
         
         //upload file from contents of input form text area
         var blob = new Blob([$('#edit-sequence-input').val()], {type: 'text/plain'});
@@ -215,7 +264,7 @@
         formData.append('fileToUpload',blob,now+'.txt');
         
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", BLASTN.uploadPath, true);
+        xhr.open("POST", Blastn.uploadPath, true);
         xhr.setRequestHeader('Authorization', 'Bearer ' + Agave.token);
         xhr.timeout = 0;        
         xhr.onload = function () {
@@ -223,12 +272,11 @@
             var parsedJSON = JSON.parse(xhr.response);
             parsedJSON = parsedJSON.result;
             
-            if (xhr.status === 200 || 202) {
-            
-                //BLASTN.inputs.query = parsedJSON._links.self.href;
+            if (xhr.status === 200 || 202) {     //if the upload was good, then submit job       
                 BLASTN.inputs.query = 'agave://araport-storage-00/'+Agave.username + '/' + inputFileName;
                 console.log('submitting:');
                 console.log(BLASTN);
+                
                 //submit the job
                 var jxhr = $.ajax({
                     headers: {
@@ -238,54 +286,53 @@
                     type: 'POST',
                     url: Agave.jobs,
                     data: JSON.stringify(BLASTN), 
-
                 }).done(function (jobResponse) {  //job should have successfully submitted
                     console.log('JOB SUBMISSION DONE!');
                     console.log(jobResponse);
         
                     $('.blast-submit').addClass('hidden');
                     $('.job-monitor').removeClass('hidden');
-                    //$('.job-monitor').html(JSON.stringify(jobResponse));
                     if(jobResponse.status === 'success') {
                         //do stuff!!!
-                        BLASTN.jobId = jobResponse.result.id;
-                        BLASTN.status = jobResponse.result.status;
+                        Blastn.jobId = jobResponse.result.id;
+                        Blastn.status = jobResponse.result.status;
                         //is the job done? (unlikely)
-                        if(BLASTN.status === 'FINISHED') {
+                        if(Blastn.status === 'FINISHED') {
                             console.log('job immediately finished, yay!');
-                            BLASTN.jobFinished(jobResponse.result);
-                        } else {
-                            
-                            BLASTN.status = jobResponse.result.status;
-                            $('.job-status').html('Job Status is ' + BLASTN.status);
-                            if((BLASTN.status === 'PENDING')) {
-                                setTimeout(function() { BLASTN.checkJobStatus(Agave.token, BLASTN.jobId); }, 10000);
+                            Blastn.jobFinished(jobResponse.result);
+                        } else { //more likely we need to poll the status   
+                            Blastn.status = jobResponse.result.status;
+                            $('.job-status').html('Job Status is ' + Blastn.status);
+                            if((Blastn.status === 'PENDING')) {
+                                setTimeout(function() { Blastn.checkJobStatus(Agave.token, Blastn.jobId); }, 10000);
                             }
                         }
                     } else {
                         //failure
+                        //todo
                         console.log('jobResponse.status was not success')
                     }
         
                 })
                 .fail(function (msg) {
+                    //todo
                     console.log('FAILED TO SUBMIT JOB! Sent:');
                     console.log(BLASTN);
                     console.log(msg);
                 });            
             } else {
+                //todo
                 console.log('something still went wrong with the file upload!!');
                 console.log(uploadResponse);
             }
-            
         }
         xhr.onerror = function () {
+            //todo
             console.log('booo! upload failed!' + xhr.status);
             console.log(xhr);
         }
-        
-        xhr.send(formData);       
-        
+       
+        xhr.send(formData); //actually make the file upload call
     });
 
 })(window, jQuery);
